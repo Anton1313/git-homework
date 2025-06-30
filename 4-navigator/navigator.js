f7eb437ab5c43c04f9930afa8c13ce320434a7f2
Anton <anton.t@railsmuffin.com>
2025-06-30 22:34:49 +0300

    dz - navigator p2

--- 4-navigator/navigator.js
+++ 4-navigator/navigator.js
@@ -1,6 +1,7 @@
-let addressLat;
-let addressLong;
-let positionLat;
-let positionLong;
+let addressLat = 10 * (Math.PI/180);
+let addressLong = 20 * (Math.PI/180);
+let positionLat = 30 * (Math.PI/180);
+let positionLong = 40 * (Math.PI/180);
 
-let distance = (((addressLat - positionLat)**2) + ((addressLong - positionLong)**2))**(1/2)
+let a = Math.sin((addressLat-positionLat)/2)**2 + Math.cos(positionLat) * Math.cos(addressLat) * Math.sin(addressLong-positionLong)**2
+let result = 2 * 6371 * Math.asin(a**(1/2))

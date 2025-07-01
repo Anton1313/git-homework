e41c4582d1d931ed94779d023595935e2713c824
Anton <anton.t@railsmuffin.com>
2025-07-01 15:59:18 +0300

    language 2

--- 5-languages/language.js
+++ 5-languages/language.js
@@ -3,12 +3,15 @@ const answer = prompt('Choose language (en, ru, de):')
 switch (answer) {
 	case 'en':{
 		console.log('Good day!')
+		break;
 	}
 	case 'ru':{
 		console.log('Добрый день!')
+		break;
 	}
 	case 'de':{
 		console.log('Gutten tag!')
+		break;
 	}
 	default:{
 		console.log('Non-supported language')

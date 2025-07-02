896881609b19e5255352a4197f53eeb217f5a793
Anton <anton.t@railsmuffin.com>
2025-07-02 15:42:25 +0300

    converter 1

---
+++ 7-converter/converter.js
@@ -0,0 +1,23 @@
+function converter(sum, input, output){
+	function getTax(val1,val2){
+		switch (true) {
+			case ((val1==='rub') && (val2==='dol')):
+				return 0.013;
+			case ((val1==='dol') && (val2==='rub')):
+				return 80;
+			case ((val1==='dol') && (val2==='eur')):
+				return 0.85;
+			case ((val1==='eur') && (val2==='dol')):
+				return 1.18;
+			case ((val1==='eur') && (val2==='rub')):
+				return 100;
+			case ((val1==='rub') && (val2==='eur')):
+				return 0.011;
+			default:
+				return null;
+		}
+	}
+	
+	let tax = getTax(input, output);
+	return sum * tax
+}

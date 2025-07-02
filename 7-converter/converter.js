a3abb18bc1379c32fc89ee40f180994a818549d2
Anton <anton.t@railsmuffin.com>
2025-07-02 16:37:42 +0300

    converter 3

--- 7-converter/converter.js
+++ 7-converter/converter.js
@@ -1,27 +1,29 @@
-function converter(sum, input, output){
+function converter(str){
+	let array = str.split(' ')
+
 	function getTax(val1,val2){
 		switch (true) {
-			case ((val1==='rub') && (val2==='dol')):
+			case ((val1==='руб') && (val2==='$')):
 				return 0.013;
-			case ((val1==='dol') && (val2==='rub')):
+			case ((val1==='$') && (val2==='руб')):
 				return 80;
-			case ((val1==='dol') && (val2==='eur')):
+			case ((val1==='$') && (val2==='€')):
 				return 0.85;
-			case ((val1==='eur') && (val2==='dol')):
+			case ((val1==='€') && (val2==='$')):
 				return 1.18;
-			case ((val1==='eur') && (val2==='rub')):
+			case ((val1==='€') && (val2==='руб')):
 				return 100;
-			case ((val1==='rub') && (val2==='eur')):
+			case ((val1==='руб') && (val2==='€')):
 				return 0.011;
 			default:
 				return null
 		}
 	}
 	
-	let tax = getTax(input, output);
+	let tax = getTax(array[1], array[2]);
 	
 	if (tax) {
-		return sum * tax
+		return array[0] * tax
 	}
 	 return tax
 }

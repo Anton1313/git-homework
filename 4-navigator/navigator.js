3c6a5ef9a17b13006b272c50542bb0a3dd9d8a2e
Anton <anton.t@railsmuffin.com>
2025-07-04 22:03:22 +0300

    navigator 4

--- 4-navigator/navigator.js
+++ 4-navigator/navigator.js
@@ -3,10 +3,10 @@ let addressLong = 20 * (Math.PI/180);
 let positionLat = 30 * (Math.PI/180);
 let positionLong = 40 * (Math.PI/180);
 
-let dLat = addressLat - positionLat;
-let dLon = addressLong - positionLong;
+let dLat = (addressLat - positionLat)/2;
+let dLon = (addressLong - positionLong)/2;
 
-let a = Math.sin(dLat/2)**2 + Math.cos(positionLat) * Math.cos(addressLat) * Math.sin((dLon/2))**2
+let a = Math.sin(dLat)**2 + Math.cos(positionLat) * Math.cos(addressLat) * Math.sin(dLon)**2
 
 let c = 2 * Math.asin(Math.sqrt(a));
 let R = 6371; // Радиус Земли в километрах

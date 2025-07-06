949c343bbe3a6aae841615cd5d456ff37ab0c2a4
Anton <anton.t@railsmuffin.com>
2025-07-06 18:41:34 +0300

    crypto 2

--- 8-crypto/crypto.js
+++ 8-crypto/crypto.js
@@ -1,5 +1,13 @@
 function crypto (str) {
-	return str.split('').reverse().join('')
+	let part1 = str.split('').slice(0,str.length/2).reverse().join('')
+	let tempPart2 = str.split('').slice(str.length/2,str.length)
+
+	let popEl = tempPart2.pop()
+	let shiftEl = tempPart2.shift()
+	tempPart2.unshift(popEl)
+	tempPart2.push(shiftEl)
+	let part2 = tempPart2.join('')
+	return part1 + part2
 }
 
 function check(secretP, cleanP){
@@ -8,6 +16,6 @@ function check(secretP, cleanP){
 
 
 console.log(crypto('password'))
-console.log(check('drowssap', 'password'))
+console.log(check('ssapdorw', 'password')) 
 console.log(check('wordpass', 'password'))
 

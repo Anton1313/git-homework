49e476a8691f41201770f2d0b4d40a3755793778
Anton <anton.t@railsmuffin.com>
2025-07-08 15:45:23 +0300

    arr 2

--- 10-array-clean/clean.js
+++ 10-array-clean/clean.js
@@ -1,8 +1,9 @@
 function filt (arr, fn){
+	let arr2;
 	for (const el of arr){
-		if (fn(el)){
-			arr.splice(arr.indexOf(el), 1)
+		if (!fn(el)){
+			arr2.push(el)
 		}
 	}
-	return arr;
+	return arr2;
 }

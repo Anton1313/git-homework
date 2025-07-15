c33ff51207753791c5f5b7d0a0332a9ad0ebdab0
Anton <anton.t@railsmuffin.com>
2025-07-15 23:22:13 +0300

    luna 3

--- 12-luna/luna.js
+++ 12-luna/luna.js
@@ -1,7 +1,7 @@
 function luna(card) {
 	const cleanCard = card.replaceAll('-','').split('').reverse()
 	const lunaCard =  cleanCard.map((num,i) => {
-		if(i % 2 === 0){
+		if(i % 2 === 1){
 			if ((Number(num) * 2) > 9){
 				num = (Number(num) * 2) - 9
 			} else {

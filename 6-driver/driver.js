fc9eb7e3a53fda5d7a11d5847cd84f6fb2b23160
Anton <anton.t@railsmuffin.com>
2025-07-21 08:53:14 +0300

    driver fix4

--- 6-driver/driver.js
+++ 6-driver/driver.js
@@ -2,6 +2,4 @@ const hasLicence = true;
 const age = 18;
 const isDrunk = false
 
-console.log(`${(age >= 18)
-	&& hasLicence
-	&& !isDrunk ? 'может' : 'не может'}`)
+console.log((age >= 18 && hasLicence && !isDrunk) ? 'может' : 'не может');

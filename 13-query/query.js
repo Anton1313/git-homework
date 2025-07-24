3f3a4fef26516626b4e3452ee5f6b38558ee3cd8
Anton <anton.t@railsmuffin.com>
2025-07-24 11:43:07 +0300

    query fix2

--- 13-query/query.js
+++ 13-query/query.js
@@ -7,10 +7,10 @@ function str(object) {
 	const arr = Object.entries(object)
 
 	const mappedArr = arr.map(element => {
-		return `${element[0]}=${element[1]}`	
+		return `${element[0]}=${element[1]}`
 		})
 	
-	let result = `${mappedArr[0]}&${mappedArr[1]}`
+	let result = mappedArr.join('&')
 	
 	return result
 }

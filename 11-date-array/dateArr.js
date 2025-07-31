01d89ee29752470aa9d0bc3ba90ed8ff1075d8a5
Anton <anton.t@railsmuffin.com>
2025-07-31 22:43:48 +0300

    array fix1

--- 11-date-array/dateArr.js
+++ 11-date-array/dateArr.js
@@ -5,17 +5,26 @@ const arr = [
 	'00/13/2022', 
 	'41/12/2023']
 
-	arr.map(el => {
-		const day = el[0]+el[1]
-		const month = el[3]+el[4]
-		const year = el[6]+el[7]+el[8]+el[9]
+	const newArr = []
 
-		if(year !== NaN){
-			if ((day > 0) && (day <= 31)){
-				if((month > 0) && (month <= 12)){
-					const res = `${day}-${month}-${year}`
-					console.log('res = ',res)
+	function dateArr (array){
+		array.filter(el => el.length = 10).map(el => {
+			const day = el.slice(0,2)
+			const month = el.slice(3,5)
+			const year = el.slice(6,el.length)
+	
+			if(year !== NaN){
+				if ((day > 0) && (day <= 31)){
+					if((month > 0) && (month <= 12)){
+						const res = `${day}-${month}-${year}`
+						newArr.push(res)
+					}
 				}
 			}
-		}
-	})
+		})
+		return newArr
+	}
+
+
+
+	console.log(dateArr(arr))
